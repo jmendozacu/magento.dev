42ebda4babde4f1cb0e5259b86bf4b270416ce92
Miguel <miguelangelramirez@me.com>
2017-04-12 07:29:24 -0400

    Finishing the header and starting the footer, header still doesn't have the mini cart showing up

--- skin/frontend/bookstore/default/js/jquery.scripts.js
+++ skin/frontend/bookstore/default/js/jquery.scripts.js
@@ -1,5 +1,23 @@
 // This is important!
 jQuery.noConflict();
 jQuery(document).ready(function() {
-    // Insert your scripts here
+    jQuery('.navbar .parent').addClass('dropdown');
+    jQuery('.navbar a.level-top').addClass('dropdown-toggle');
+    jQuery('.navbar li.parent ul').addClass('dropdown-menu');
+    jQuery('.navbar li.level1 ul').wrap('<li class="dropdown-submenu"/>');
+    jQuery('.navbar ul.nav li.level0.dropdown').hover(function() {
+        jQuery(this).find('.level0.dropdown-menu').stop(true, true).fadeIn();
+    }, function() {
+        jQuery(this).find('.level0.dropdown-menu').stop(true, true).fadeOut();
+    });
+
+
+
+
+
+
+
+
+
+
 });
